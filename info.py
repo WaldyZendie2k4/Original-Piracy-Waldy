f914633a6904c229e461638d688c73c83f80f6df
Jith2252 <125337761+Jith2252@users.noreply.github.com>
2023-05-08 10:40:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -68,8 +68,8 @@ UPSTREAM_REPO = "https://github.com/prajith2252/url-filebot"
     
       # URL Shortener #
 
-URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'hitshort.in')
-URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', '8a9aa6953ccbb22479dee67bc5fc41efedf7852d')
+URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'xpshort.com')
+URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', '849d8b7dd6ae0fd261f4c39be20d86061967a8c2')
 
      # Auto Delete For Group Message (Self Delete) #
 SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 300))
