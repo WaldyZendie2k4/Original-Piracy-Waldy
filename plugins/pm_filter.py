b1e16811af65ea4ce92c9698c5e44196bfae5e14
batmanbehindthedark <129989819+batmanbehindthedark@users.noreply.github.com>
2023-05-27 10:53:45 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -687,7 +687,7 @@ async def auto_filter(client, msg, spoll=False):
 
     btn.insert(0,
         [
-            InlineKeyboardButton(text="⚡ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ⚡", url='https://t.me/how_to_use_tmafilesbot/10')
+            InlineKeyboardButton(text="⚡ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ⚡", url='https://t.me/KumarValimaiOfcl')
         ]
     )
 
