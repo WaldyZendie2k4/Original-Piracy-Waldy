90739a28debc2720e65869e70adc46af2ae26cd4
batmanbehindthedark <129989819+batmanbehindthedark@users.noreply.github.com>
2023-06-01 20:31:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -63,13 +63,13 @@ LOG_STR += ("Long IMDB storyline enabled." if LONG_IMDB_DESCRIPTION else "LONG_I
 LOG_STR += ("Spell Check Mode Is Enabled, bot will be suggesting related movies if movie not found\n" if SPELL_CHECK_REPLY else "SPELL_CHECK_REPLY Mode disabled\n")
 LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST_ELM} elements\n" if MAX_LIST_ELM else "Full List of casts and crew will be shown in imdb template, restrict them by adding a value to MAX_LIST_ELM\n")
 LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
-UPSTREAM_REPO = "https://github.com/bittukittu123/tgm_thankstotma"
+UPSTREAM_REPO = "https://github.com/batmanbehindthedark/koyebtest"
 ## EXTRA FEATURES ##
     
       # URL Shortener #
 
-URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'tinyfy.in')
-URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', '2b152c93e35ef8af4a507e6921bbf526a37e46e6')
+URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'shareus.in')
+URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', 'xHWPqx7MQCNUxEF2Q57uv9ssooD2')
 
      # Auto Delete For Group Message (Self Delete) #
 SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 300))
