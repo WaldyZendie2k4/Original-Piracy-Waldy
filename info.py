d506db95426766a24c2ca4924a54f40ae66a63bc
batmanbehindthedark <129989819+batmanbehindthedark@users.noreply.github.com>
2023-06-01 17:16:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -68,8 +68,8 @@ UPSTREAM_REPO = "https://github.com/bittukittu123/tgm_thankstotma"
     
       # URL Shortener #
 
-URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'omegalinks.in')
-URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', '161d7dcb7d0fa88c25b2a7021ea9970ec4a6d9ea')
+URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'tinyfy.in')
+URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', '2b152c93e35ef8af4a507e6921bbf526a37e46e6')
 
      # Auto Delete For Group Message (Self Delete) #
 SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 300))
