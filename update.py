f23572dd094b2ba7b9324f7dee5e828ec5322c79
batmanbehindthedark <129989819+batmanbehindthedark@users.noreply.github.com>
2023-11-03 08:58:25 +0530

    Update update.py

--- update.py
+++ update.py
@@ -3,7 +3,7 @@ from os import path as ospath, environ
 from subprocess import run as srun
 from requests import get as rget
 
-UPSTREAM_REPO = "https://github.com/bittukittu123/tgm_thankstotma"#environ.get('UPSTREAM_REPO')
+UPSTREAM_REPO = "https://github.com/batmanbehindthedark/koyebtest"#environ.get('UPSTREAM_REPO')
 UPSTREAM_BRANCH = "main" 
 try:
     if len(UPSTREAM_REPO) == 0:
